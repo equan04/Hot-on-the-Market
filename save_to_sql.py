b51ad8c045c0d0154a4c76959a72ac748d054fec
equan04 <93830039+equan04@users.noreply.github.com>
2025-03-02 03:29:55 -0600

    reorganize

--- save_to_sql.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-from sqlalchemy import create_engine
-
-con = create_engine('sqlite:///company_data.db')
-
-chunks = pd.read_csv('combined_data.csv', chunksize=100000)
-for chunk in chunks:
-    chunk.to_sql(name='company_data', if_exists='append', con=con)
